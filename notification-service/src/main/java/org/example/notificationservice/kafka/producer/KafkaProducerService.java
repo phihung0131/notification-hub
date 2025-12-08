6d0d9d18e35d959551931b92f56b9ec6ba144e57
phihung0131 <phihung0131@gmail.com>
2025-12-08 20:52:22 +0700

    feat: expose debug ports and update logging levels for notification and tenant services

--- notification-service/src/main/java/org/example/notificationservice/kafka/producer/KafkaProducerService.java
+++ notification-service/src/main/java/org/example/notificationservice/kafka/producer/KafkaProducerService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.example.events.NotificationEvent;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -12,7 +13,7 @@ public class KafkaProducerService {
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public void sendMessage(NotificationEvent eventDto) {
-        log.debug("Sending message to kafka: {}", eventDto);
+        log.info("Sending message to kafka: {}", eventDto);
         kafkaTemplate.send("notification-topic", eventDto);
     }
 }
